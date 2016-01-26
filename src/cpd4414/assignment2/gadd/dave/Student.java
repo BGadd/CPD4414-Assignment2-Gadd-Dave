fd8293a8b63c62ec95e572b7a3a4e0de5f304319
c0641903 <c0641903@SB-N208-27.lambton.on.ca>
2016-01-26 09:18:16 -0500

    Created Student and Course class files.

---
+++ src/cpd4414/assignment2/gadd/dave/Student.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment2.gadd.dave;
+
+/**
+ *
+ * @author c0641903
+ */
+public class Student {
+    
+}
