55129f10f341f92a55890494cd2851d23e9918f3
c0641903 <c0641903@SB-N208-27.lambton.on.ca>
2016-01-26 10:02:56 -0500

    Created Student and Course class skeletons.

--- src/cpd4414/assignment2/gadd/dave/Course.java
+++ src/cpd4414/assignment2/gadd/dave/Course.java
@@ -5,10 +5,65 @@
  */
 package cpd4414.assignment2.gadd.dave;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author c0641903
  */
 public class Course {
+    private List<Student> students;
     
+    public Course() {
+        
+    }
+    public Course(List<Student> studentList) {
+        
+    }
+    
+    public void add(Student student) {
+        
+    }
+    
+    public void remove(Student student) {
+        
+    }
+    public void remove(String id) {
+        
+    }
+    public void remove(int position) {
+        
+    }
+    
+    public void insert(Student student, int position) {
+        
+    }
+    
+    public Student get(String id) {
+        
+    }
+    public Student get(int position) {
+        
+    }
+    public List<Student> getAll() {
+        
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+    }
+    @Override
+    public String toString() {
+        
+    }
+    
+    public Set<Student> getAllByGender(String gender) {
+        
+    }
+    public Map<String, Set<Student>> getGradeMap() {
+        
+    }
 }
--- src/cpd4414/assignment2/gadd/dave/Student.java
+++ src/cpd4414/assignment2/gadd/dave/Student.java
@@ -10,5 +10,52 @@ package cpd4414.assignment2.gadd.dave;
  * @author c0641903
  */
 public class Student {
+    private String name;
+    private String id;
+    private String gender;
+    private Double grade;
     
+    public Student() {
+        
+    }
+    public Student(String name, String id, String gender, double grade) {
+        
+    }
+
+    public String getName() {
+        
+    }
+    public void setName(String name) {
+        
+    }
+    
+    public String getId() {
+        
+    }
+    public void setId(String id) {
+        
+    }
+    
+    public String getGender() {
+        
+    }
+    public void setGender(String gender) {
+        
+    }
+    
+    public Double getGrade() {
+        
+    }
+    public void setGrade(Double grade) {
+        
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+    }
+    @Override
+    public String toString() {
+        
+    }
 }
