8becb17158a76f10c2d00a3858e687ffc0e5a1b9
c0641903 <c0641903@SB-N208-12.lambton.on.ca>
2016-01-26 12:51:47 -0500

    Built Student class methods.

--- src/cpd4414/assignment2/gadd/dave/Student.java
+++ src/cpd4414/assignment2/gadd/dave/Student.java
@@ -16,46 +16,57 @@ public class Student {
     private Double grade;
     
     public Student() {
-        
+        name = "";
+        id = "";
+        gender = "";
+        grade = 0.00;
     }
     public Student(String name, String id, String gender, double grade) {
-        
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
     }
 
     public String getName() {
-        
+        return name;
     }
     public void setName(String name) {
-        
+        this.name = name;
     }
     
     public String getId() {
-        
+        return id;
     }
     public void setId(String id) {
-        
+        this.id = id;
     }
     
     public String getGender() {
-        
+        return gender;
     }
     public void setGender(String gender) {
-        
+        this.gender = gender;
     }
     
     public Double getGrade() {
-        
+        return grade;
     }
     public void setGrade(Double grade) {
-        
+        this.grade = grade;
     }
     
     @Override
     public boolean equals(Object obj) {
-        
+        if (obj instanceof Student) {
+            Student student = (Student) obj;
+            return (student.getName().equals(name) && student.getId().equals(id));
+        } else {
+            return false;
+        }
     }
     @Override
     public String toString() {
-        
+        return "{ \"name\" : \"" + name + "\", \"id\" : \"" + id + "\", \"gender\" : \"" + gender + "\", \"grade\" : " + grade + " }";
     }
 }
